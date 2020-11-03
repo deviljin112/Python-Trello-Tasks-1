a3932b47402d3c5748d2a811ac57df3e3335f32c
D3v <deviljin112@users.noreply.github.com>
2020-11-03 19:31:14 +0000

    add pseudo

--- task_1.py
+++ task_1.py
@@ -1,30 +1,71 @@
+# Import datetime module for operations on date and time
 import datetime
 
+# Take in inputs from the user about their name, age and birthday
 name = input("What's your name stranger?\n=> ").capitalize()
 age = int(input("How old are you?\n=> "))
+
+# This retrives the input and splits it as a space
+# It uses multiple variable assignment instead of creating a list
+# It is assumed that the user will follow the formatting an only two values will be given
 day, month = input("When is your birthday?\nFormat: DD MM\n=> ").split(" ")
+
+# Because they are dates we want to change the strings into integers
 day, month = int(day), int(month)
 
+# Get the current date
 today = datetime.datetime.now()
+
+# Find out the year user was born
 user_year = today.year - age
+
+# Format a datetime instance of users dob
 birthday = datetime.datetime(user_year, month, day)
+
+# Create a bool to track whether a user had birthday or not
 had_birthday = True
 
+# Get the value of days until the birthday
+# (positive means its after today's day, negative means its before today's day)
+# Day being positive or negative does not mean that the birthday occured
+# As birthday may be on 1st of October but today its 21st of March therefore "days_left" will be -20
+# This difference is the amount of days until the actual birthday
+days_left = today.day - day
+
+# Check whether the user had their birthday or not
 if today.month - month < 0:
+    # Get the amount of months until birthday into a variable
     months_left = month - today.month
+
+    # Set the bool to false as the user didn't have their birthday yet
     had_birthday = False
 
-days_left = today.day - day
-if days_left < 0:
-    days_left = days_left * -1
+    # If days left is negative, make it a positive value for clarity
+    if days_left < 0:
+        days_left *= -1
 
+# If the birthday is this month, and days left is not negative (i.e. the birthday didnt happen yet)
+elif today.month == month and days_left < 0:
+
+    # Months left is 0 as its this month
+    months_left = 0
+
+    # Set days left to positive as before
+    days_left *= -1
+
+    # Set the bool to false like before
+    had_birthday = False
+
+# Find out the difference in deltatime between today and dob
 difference = today - birthday
 
+# Find out how many hours, minutes and seconds has elapsed since user's birth
 hours_in_days = difference.days * 24
 hours = (difference.seconds // 3600) + hours_in_days
 minutes = (difference.seconds // 60) % 60
 seconds = int(difference.seconds % 60)
 
+# Print out all the main information about the user
 print(
     f"""
 OMG {name}!
@@ -37,6 +78,8 @@ You've been around for
 """
 )
 
+# Birthday specific print
+# This print depends whether the user had their birthday or not
 if not had_birthday:
     print(f"You have {days_left} days and {months_left} months until your birthday!")
 else:
--- task_2.py
+++ task_2.py
@@ -1,21 +1,45 @@
-import random
-
+# Create a loop that wil re-occur 10 times
+# Uses "_" as the actual return value of the range function is irrelevant
 for _ in range(10):
     print("Hello!")
 
+# Creates an empty list ready for new data to be added
 list_names = []
+
+# Similarly to before loop will re-occur 5 times
 for _ in range(5):
+
+    # Instead of creating seperate variable for the input
+    # It is instantly added to the list
+    # Input will be overriden on each loop anyway
+    # therefore doesnt need to be stored in memory
     list_names.append(input("Please input a name\n=> "))
+
 print(list_names)
 
+# Creates another empty list
 list_name_lower = []
+
+# Loop over each value in the `list_names` list
 for v in list_names:
+
+    # Add the lowercase version of the value from the iterated list
     list_name_lower.append(v.lower())
+
 print(list_name_lower)
 
+# Creates another empty list
 even_strings = []
+
+# Loop over each value in the `list_name_lower` list
 for i in list_name_lower:
+
+    # Checks the length of the string
+    # Checks whether the number is divisible by 2
+    # If it is then it must be even
     if len(i) % 2 == 0:
+
+        # Even strings are added to an empty list
         even_strings.append(i)
 
 print(even_strings)
